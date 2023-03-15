39b90be53fd99f4136a72760ef6ed01667677964
Jacek Wojtuszek <jacek.wojtuszek@gmail.com>
2023-03-15 22:37:07 +0100

    Rozwiązanie

--- src/main/java/pl/javastart/euler2/Euler2.java
+++ src/main/java/pl/javastart/euler2/Euler2.java
@@ -4,5 +4,29 @@ public class Euler2 {
 
     public static void main(String[] args) {
 
+        Euler2 euler2 = new Euler2();
+        int result = euler2.sumEvenFibNumBelow(4000000);
+        System.out.println(result);
+    }
+
+    public int sumEvenFibNumBelow(int limit) {
+        boolean sumBelow = false;
+        int sum = 0;
+        int evenSum = 2;
+        int firstNumber = 1;
+        int secondNumber = 2;
+        while (!sumBelow) {
+            if (sum <= limit) {
+                sum = firstNumber + secondNumber;
+                firstNumber = secondNumber;
+                secondNumber = sum;
+                if (sum % 2 == 0) {
+                    evenSum += sum;
+                }
+            } else {
+                sumBelow = true;
+            }
+        }
+        return evenSum;
     }
 }
---
+++ src/test/java/pl/javastart/euler2/Euler2Test.java
@@ -0,0 +1,21 @@
+package pl.javastart.euler2;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Euler2Test {
+
+    @ParameterizedTest
+    @CsvSource(value = {"10, 10", "34, 44", "144, 188"})
+    void shouldReturnExpectedValue(int limit, int expected) {
+        //given
+        Euler2 euler2 = new Euler2();
+
+        //when
+        int result = euler2.sumEvenFibNumBelow(limit);
+
+        //then
+        assertEquals(expected, result);
+    }
+}
